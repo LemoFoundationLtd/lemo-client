1ef538b6584578c83bb3d7463044c8c7161e3234
yAmanda <amandayang@inslabs.org>
2019-10-24 15:17:52 +0800

    fix: modify getTermReward

--- test/datas.js
+++ test/datas.js
@@ -705,16 +705,9 @@ export const deputyNodes = [
 ]
 export const formattedDeputyNodes = [
     {
-        minerAddress: 'Lemo83DZ5J99JSK5ZH89TCW7T6ZZCWJ8H7FDGA7W',
-        incomeAddress: 'Lemo83DZ5J99JSK5ZH89TCW7T6ZZCWJ8H7FDGA7W',
-        nodeID: '0x0e7dcd418dbe7717eb0e83162f8810a3c7725e0d386b324dc5f3ef5a27a2a83e393a193f6ab53d3a51b490adeee362357676f50eed3d188824ef1fb3af02b2d0',
+        ...deputyNodes[0],
         rank: 0,
-        votes: '50000',
-        host: '127.0.0.1',
         port: 8080,
-        depositAmount: '5000000000000000000000000',
-        introduction: 'ddf',
-        p2pUri: '0e7dcd418dbe7717eb0e83162f8810a3c7725e0d386b324dc5f3ef5a27a2a83e393a193f6ab53d3a51b490adeee362357676f50eed3d188824ef1fb3af02b2d0@127.0.0.1:8080',
     },
 ]
 
